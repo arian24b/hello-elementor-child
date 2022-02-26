b84a3ed65e868c25e4b3b7c3357c7f65c73b9a87
arianomrani <arianomrani24@gmail.com>
2022-02-26 12:19:37 +0330

    bug fix

--- functions.php
+++ functions.php
@@ -13,7 +13,7 @@ function hello_elementor_child_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style'));
 }
-add_action( 'wp_enqueue_scripts', 'hello_elementor_child_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'hello_elementor_child_enqueue_styles', 20 );
 
 // remove wp version number from scripts and styles 
 function remove_css_js_version( $src ) {
